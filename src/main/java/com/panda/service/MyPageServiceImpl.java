896d07515eae70155693cfd830ade91d691f561a
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2022-12-30 16:19:03 +0900

    12/30 해민 서비스 어노테이션 추가

---
+++ src/main/java/com/panda/service/MyPageServiceImpl.java
@@ -0,0 +1,18 @@
+package com.panda.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.panda.domain.MemberVO;
+
+@Service
+public class MyPageServiceImpl implements MyPageService {
+
+	@Override
+	public List<MemberVO> getList() {
+		
+		return null;
+	}
+
+}
