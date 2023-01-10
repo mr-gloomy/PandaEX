392509006b447ac7c62fbbde175925943ddfd9f3
kjh0707 <wlghksrj2@naver.com>
2023-01-10 17:56:21 +0900

    김지환 0110

--- src/main/java/com/panda/controller/MemberController.java
+++ src/main/java/com/panda/controller/MemberController.java
@@ -195,6 +195,7 @@ public class MemberController {
 		
 		service.insertRep(vo);
 		
+		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out=response.getWriter();
 		out.println("<script>");
