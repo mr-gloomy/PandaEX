da305f41d63b583ab9cb7882d3bf2194aa66891f
ITWILL <ITWILL@DESKTOP-FKD5EKF>
2023-01-12 15:17:44 +0900

    중혁

--- src/main/java/com/panda/controller/PayController.java
+++ src/main/java/com/panda/controller/PayController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -58,6 +59,12 @@ public class PayController {
 	@GetMapping("/kakao")
 	public String kakaoPay() {
 		return "payment/paymentReady";
+	}
+	
+	// http://localhost:8080/payment/pay_page
+	@RequestMapping(value = "/pay_page",method = RequestMethod.GET)
+	public void pay_page() throws Exception {
+		
 	}
 	
 	@PostMapping("/charge")
